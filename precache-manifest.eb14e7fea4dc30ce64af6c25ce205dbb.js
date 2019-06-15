3369cc64924d2490a60b5e32689aac9ae9b6ea8d
Sneha Kalita <sneha3950@gmail.com>
2019-06-15 22:54:38 +0530

    Updates

---
+++ precache-manifest.eb14e7fea4dc30ce64af6c25ce205dbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f0a633ea42a96faca5f14d18e2fc79",
+    "url": "/rest-countries/index.html"
+  },
+  {
+    "revision": "7996cdf70c63a264518e",
+    "url": "/rest-countries/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/rest-countries/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "7996cdf70c63a264518e",
+    "url": "/rest-countries/static/js/main.26efb52a.chunk.js"
+  },
+  {
+    "revision": "c5dc570e91023d6fd4b1",
+    "url": "/rest-countries/static/js/runtime~main.3e1474be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rest-countries/static/media/logo.5d5d9eef.svg"
+  }
+]);
